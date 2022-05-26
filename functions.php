2f650dc3098505f260ed6c55ab061f0f70793f6c
circlebd <a_mahfuzul@yahoo.com>
2022-05-26 18:06:25 +0600

    initial status

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+class OneListing_Travel
+{
+
+	public function __construct()
+	{
+		add_action('after_setup_theme', array($this, 'onelisting_child_theme_setup'));
+		$this->includes();
+	}
+
+	public function onelisting_child_theme_setup()
+	{
+		load_child_theme_textdomain('onelisting', get_stylesheet_directory() . '/languages');
+	}
+
+	public function includes()
+	{
+		add_action('wp_enqueue_scripts', array($this, 'onelisting_child_styles'), 18);
+	}
+
+	public function onelisting_child_styles()
+	{
+		wp_enqueue_style('onelisting-child-style', get_stylesheet_uri());
+	}
+}
+new OneListing_Travel;
